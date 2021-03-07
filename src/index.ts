d4802c35ba5013013f4eec684027031a1b7dcee0
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2021-03-07 12:32:58 +0100

    chore(1): add linter and formatter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { app, BrowserWindow } from 'electron';
+
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require('electron-squirrel-startup')) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
