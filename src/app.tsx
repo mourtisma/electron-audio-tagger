d049d5d7dbaec8e077847e0a1178fc5548e7c7a5
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2021-03-22 21:51:53 +0100

    Revert "1/boilerplate"

--- src/app.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HelloWorld from './hello-world';
-
-function render() {
-    ReactDOM.render(<HelloWorld />, document.body);
-}
-
-render();
