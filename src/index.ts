011fda62a04b8b7876bf835ec115717dfad572de
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2021-05-18 23:37:04 +0200

    chore(6): remove devtools in production

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,12 @@ const createWindow = (): void => {
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: true,
+            devTools: process.env.NODE_ENV === 'development',
         },
     });
 
     // and load the index.html of the app.
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
