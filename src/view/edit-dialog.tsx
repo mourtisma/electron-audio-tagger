7c09e19c6b39c3365ef84d7d8afc8f2e4cc6f796
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2021-11-22 23:13:41 +0100

    style(7): improve dialog styling

--- src/view/edit-dialog.tsx
+++ src/view/edit-dialog.tsx
@@ -3,6 +3,18 @@ import AudioFile from '@src/model/audio-file';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    closeButton: {
+        position: 'absolute',
+        right: '0.5em',
+        bottom: '0.5em',
+    },
+    attributes: {
+        marginLeft: '0.5em',
+    },
+});
 
 interface EditDialogProps {
     audioFile: AudioFile;
@@ -14,19 +26,25 @@ export default ({
     audioFile,
     open,
     handleClose,
-}: EditDialogProps): JSX.Element =>
-    audioFile && (
-        <Dialog onClose={handleClose} open={open}>
-            <DialogTitle>{`Edit ${audioFile.name}`}</DialogTitle>
-            <div
-                data-testid="close"
-                role="none"
-                onClick={() => {
-                    handleClose();
-                }}
-            >
-                <CloseIcon />
-            </div>
-            <p>{`Title: ${audioFile.title || ''}`}</p>
-        </Dialog>
+}: EditDialogProps): JSX.Element => {
+    const classes = useStyles();
+    return (
+        audioFile && (
+            <Dialog onClose={handleClose} open={open}>
+                <DialogTitle>{`Edit ${audioFile.name}`}</DialogTitle>
+                <div
+                    data-testid="close"
+                    role="none"
+                    onClick={() => {
+                        handleClose();
+                    }}
+                >
+                    <CloseIcon className={classes.closeButton} />
+                </div>
+                <div className={classes.attributes}>
+                    <p>{`Title: ${audioFile.title || ''}`}</p>
+                </div>
+            </Dialog>
+        )
     );
+};
